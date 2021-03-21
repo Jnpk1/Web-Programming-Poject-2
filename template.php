4064aa4d430286521e449eafad3238390897826f
mlopez44 <mlopez44@student.gsu.edu>
2021-03-21 19:02:48 -0400

    Template.php + theme.css

---
+++ template.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<link rel="stylesheet" href="maintheme.css">
+</head>
+<title> bonk </title>
+<body>
+
+<main>
+
+<div id="righttext">
+
+<br>
+<br>
+
+
+</div>
+
+</main>
+
+</body>
+</html>
